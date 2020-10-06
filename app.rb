608ab3de7ca6263a73299ae95db8f653e252cd96
Justin_Webb <Webb.n.justin@gmail.com>
2020-10-06 13:27:47 -0600

    Introduction to parameters taking and returning param 'hashes'

--- app.rb
+++ app.rb
@@ -8,11 +8,28 @@ class App < Sinatra::Base
   end
 
   # This is a sample dynamic route.
+  #: is a dynamic route notation
   get "/hello/:name" do
     @user_name = params[:name]
     "Hello #{@user_name}!"
   end
 
-  # Code your final two routes here:
+  #dynamic route to goodbye
+  get '/goodbye/:name' do
+    #stores the parameter of name given from user to person 
+    @person_name = params[:name]
+    #returns the personalized name given by user
+    "Goodbye, #{@person_name}."
+  end
+
+  #dynamic route to multiply
+  get '/multiply/:num1/:num2' do
+    #takes in the two user given numbers and saves the product as @product 
+    #.to_i is necessary to not get html error as params are strings 
+    @product = params[:num1].to_i * params[:num2].to_i
+    #returns the product to the user
+    "#{@product}"
+    #<p><%"#{@first * @second}"%></p>
+  end
 
 end
